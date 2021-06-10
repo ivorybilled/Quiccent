3bd37955107f3be1268db392500ae75d59a61aab
David Bennett <david.bennett@us.mcd.com>
2021-06-10 16:22:34 -0400

    mutex for singleton app

--- Program.cs
+++ Program.cs
@@ -1,20 +1,30 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Quiccent
 {
     static class Program
     {
+        static Mutex Mutex = new Mutex(false, "QuiccentByDavidBennett");
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            var mainForm = new Form1();
-            Application.Run();
+            if (Mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                var mainForm = new Form1();
+                Application.Run();
+            }
+            else
+            {
+                MessageBox.Show("Quiccent is already running in another instance.");
+            }
         }
     }
 }
